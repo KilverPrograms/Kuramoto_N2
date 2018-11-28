0aa427066c6367c9bc05ee3ad49c4139d78e51b2
Kilver J. Campos <kilvercampos@gmail.com>
2018-11-28 13:46:46 -0400

    pyKuNet v 0.0.1

--- KMpy.py
+++ KMpy.py
@@ -1,15 +1,22 @@
-from __future__ import division
-import os
-import h5py
-import copy
-import odespy
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function
+import os, copy, h5py, odespy
 import matplotlib.pyplot as plt
 import numpy as np
 from numba import jitclass
 
 
 __author__ = "Kilver J. Campos"
+__copyright__ = "Copyright 2018, Kuramoto Model Network on Python (pyKuNet)"
+__credits__ = ["Kilver J. Campps"]
+__license__ = "GPL3"
 __version__ = "0.0.1"
+__maintainer__ = "Kilver J. Campos"
+__email__ = "kilvercampos@gmail.com"
+__status__ = "Production"
+
 
 class Kuramoto:
     def __init__(self, omega, kappa):
@@ -42,9 +49,9 @@ class Solver:
         self.solver.set_initial_condition(self.problem.ics)
         time_points = np.linspace(0., self.T, self.N + 1)
         self.u, self.t = self.solver.solve(time_points)
-        print 'Final u(t={0:4.2f})={1}'.format(self.t[-1], np.array2string(self.u[-1, :]
+        print('Final u(t={0:4.2f})={1}'.format(self.t[-1], np.array2string(self.u[-1, :]
                                                                            , precision=2, separator=', ',
-                                                                           suppress_small=True))
+                                                                           suppress_small=True)))
 
     def opr_t(self):
         self.r = np.abs((1 / float(self.problem.n_osc)) * np.sum(np.exp(1j * self.u), axis=1))
@@ -62,7 +69,7 @@ class Solver:
             np.savetxt("pase_" + str(self.problem.kappa) + ".txt", self.u)
         elif fmt == 'hdf5':
             if os.path.isfile("./phases.h5") == True:
-                print "File exist!"
+                print("File exist!")
                 phases = h5py.File("phases.h5", 'a')
                 phases.create_dataset(str(self.problem.kappa), data=self.u)
             else:
